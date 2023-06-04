bdc1276f78b769608d4d00e4e0d3985bcf8278a2
Riosereno <99235810+Riosereno@users.noreply.github.com>
2023-06-04 00:05:35 -0500

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,17 +1,15 @@
-const Director = require("./Director");
-const Movie = require("./Movie");
 const Actor = require("./Actor");
 const Genre = require("./Genre");
+const Director = require("./Director");
+const Movie = require("./Movie");
 
-Director.belongsToMany( Movie, { through: "MoviesDirectors" });
-Movie.belongsToMany(Director, { through: "MoviesDirectors" });
+
 
 Movie.belongsToMany(Actor, { through: "MoviesActors" });
 Actor.belongsToMany(Movie, { through: "MoviesActors" });
 
-Movie.belongsToMany(Genre, { through: "MoviesGenres" });
-Genre.belongsToMany(Movie, { through: "MoviesGenres" });
-
 Movie.belongsToMany(Director, { through: "MoviesDirectors" });
 Director.belongsToMany(Movie, { through: "MoviesDirectors" });
 
+Movie.belongsToMany(Genre, { through: "MoviesGenres" });
+Genre.belongsToMany(Movie, { through: "MoviesGenres" });
